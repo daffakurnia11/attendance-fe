020141ac58be00d2264b43741c36478a0c601144
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-10-12 18:44:42 +0700

    feat(typography-component): :sparkles: Create Typography component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { inter } from "@/utils/fonts";
 import "@/styles/globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+      </body>
     </html>
   );
 }
